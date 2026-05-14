398fbdb22118994753919a187a58a0a9c84af3af
SivaGuduri <sivaguduri@eworldes.com>
2026-05-14 15:52:04 +0530

    Fix: path for production

--- backend/predictor/urls.py
+++ backend/predictor/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import home, predict
+from predictor.views import home, predict
 
 urlpatterns = [
-    path("", home),
-    path("predict/", predict),
+    path('', home),
+    path('predict/', predict),
 ]
